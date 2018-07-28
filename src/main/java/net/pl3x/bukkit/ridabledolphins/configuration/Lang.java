2e367f163bc2c8c7b2130901286f66e190b13512
BillyGalbreath <Blake.Galbreath@GMail.com>
2018-07-28 05:14:49 -0500

    Implement collecting fish in a water bucket

--- src/main/java/net/pl3x/bukkit/ridabledolphins/configuration/Lang.java
+++ src/main/java/net/pl3x/bukkit/ridabledolphins/configuration/Lang.java
@@ -7,11 +7,16 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Lang {
     public static String COMMAND_NO_PERMISSION;
+    public static String COLLECT_NO_PERMISSION;
     public static String RIDE_NO_PERMISSION;
     public static String SHOOT_NO_PERMISSION;
+    public static String BUCKET_NAME;
+    public static List<String> BUCKET_LORE = new ArrayList<>();
     public static String VERSION;
     public static String RELOAD;
 
@@ -25,10 +30,18 @@ public class Lang {
         FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
 
         COMMAND_NO_PERMISSION = config.getString("command-no-permission", "&cYou do not have permission for that command!");
+        COLLECT_NO_PERMISSION = config.getString("collect-no-permission", "&cYou do not have permission to collect dolphins!");
         RIDE_NO_PERMISSION = config.getString("command-no-permission", "&cYou do not have permission to ride!");
         SHOOT_NO_PERMISSION = config.getString("command-no-permission", "&cYou do not have permission to shoot!");
         VERSION = config.getString("version", "&d{plugin} v{version}.");
         RELOAD = config.getString("reload", "&d{plugin} v{version} reloaded config.");
+
+        BUCKET_NAME = config.getString("bucket-name", "&bBucket of Dolphin");
+        String lore = config.getString("bucket-lore", "This bucket contains a dolphin!\nRight click in water to place");
+        BUCKET_LORE.clear();
+        for (String line : lore.split("\n")) {
+            BUCKET_LORE.add(ChatColor.translateAlternateColorCodes('&', line));
+        }
     }
 
     public static void send(CommandSender recipient, String message) {
--- src/main/java/net/pl3x/bukkit/ridabledolphins/listener/DolphinListener.java
+++ src/main/java/net/pl3x/bukkit/ridabledolphins/listener/DolphinListener.java
@@ -1,17 +1,96 @@
 package net.pl3x.bukkit.ridabledolphins.listener;
 
 import net.pl3x.bukkit.ridabledolphins.configuration.Lang;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.spigotmc.event.entity.EntityDismountEvent;
 
 public class DolphinListener implements Listener {
+    private final ItemStack dolphin_bucket = new ItemStack(Material.COD_BUCKET, 1);
+
+    public DolphinListener() {
+        ItemMeta meta = dolphin_bucket.getItemMeta();
+        meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', Lang.BUCKET_NAME));
+        meta.setLore(Lang.BUCKET_LORE);
+        meta.setUnbreakable(true);
+        meta.addEnchant(Enchantment.DURABILITY, 10, true);
+        meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_ENCHANTS);
+        dolphin_bucket.setItemMeta(meta);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onCollectDolphin(PlayerInteractAtEntityEvent event) {
+        Entity dolphin = event.getRightClicked();
+        if (dolphin.getType() != EntityType.DOLPHIN) {
+            return; // not a dolphin
+        }
+
+        if (!dolphin.getPassengers().isEmpty()) {
+            return; // dolphin has a rider
+        }
+
+        Player player = event.getPlayer();
+        ItemStack bucket = player.getInventory().getItem(event.getHand());
+        if (bucket.getType() != Material.WATER_BUCKET) {
+            return; // not a water bucket
+        }
+
+        Entity vehicle = player.getVehicle();
+        if (vehicle != null && vehicle.getUniqueId().equals(dolphin.getUniqueId())) {
+            return; // player is riding this dolphin
+        }
+
+        if (!player.hasPermission("allow.dolphin.collect")) {
+            Lang.send(player, Lang.COLLECT_NO_PERMISSION);
+            return;
+        }
+
+        player.getInventory().setItem(event.getHand(), dolphin_bucket.clone());
+        dolphin.remove();
+        event.setCancelled(true);
+    }
+
     @EventHandler
+    public void onPlaceDolphin(PlayerBucketEmptyEvent event) {
+        if (event.getBucket() != Material.COD_BUCKET) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+        EquipmentSlot hand = EquipmentSlot.HAND;
+        ItemStack bucket = player.getInventory().getItemInMainHand();
+        if (!bucket.isSimilar(dolphin_bucket)) {
+            hand = EquipmentSlot.OFF_HAND;
+            bucket = player.getInventory().getItemInOffHand();
+            if (!bucket.isSimilar(dolphin_bucket)) {
+                return;
+            }
+        }
+
+        if (player.getGameMode() != GameMode.CREATIVE) {
+            bucket.setAmount(Math.max(0, bucket.getAmount() - 1));
+            player.getInventory().setItem(hand, bucket);
+        }
+
+        player.getWorld().spawnEntity(event.getBlockClicked().getRelative(event.getBlockFace()).getLocation().add(0.5, 0.5, 0.5), EntityType.DOLPHIN);
+        event.setCancelled(true); // do not spawn a cod!
+    }
+
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onClickDolphin(PlayerInteractAtEntityEvent event) {
         if (event.getHand() != EquipmentSlot.HAND) {
             return; // dont fire twice
